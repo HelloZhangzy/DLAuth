9dac9ce32f608494ea6c5e0c9b0b3b32c17b1f2b
zzy <loginzzy0709@163.com>
2017-12-06 22:59:32 +0800

    1206

---
+++ Src/DLiteAuthFrame/DLiteAuthFrame.Web/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using DLiteAuthFrame.APP.IApp;
+using DLiteAuthFrame.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DLiteAuthFrame.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private IAuthApp Auth = null;
+
+        public AccountController(IAuthApp _auth)
+        {
+            Auth = _auth;
+        }
+
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string Name, string PassWord)
+        {
+            if (Auth.Login(Name, PassWord))
+                return Content(new AjaxResult { state = ResultType.success.ToString(),Url="/Home/Index", message = "登录成功。" }.ToJson());
+            else
+                return Content(new AjaxResult { state = ResultType.error.ToString(), message = "账号或密码错误！" }.ToJson());
+            //return View();
+        }
+        [HttpPost]
+        public ActionResult Login2()
+        {
+            return Content(new AjaxResult { state = ResultType.error.ToString(), message = "账号或密码错误！" }.ToJson());
+        }
+    }
+}
--- Src/DLiteAuthFrame/DLiteAuthFrame.Web/Controllers/LoginController.cs
+++ Src/DLiteAuthFrame/DLiteAuthFrame.Web/Controllers/LoginController.cs
@@ -11,6 +11,14 @@ using System.Web.Mvc;
 
 namespace DLiteAuthFrame.Web.Controllers
 {
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Gender { get; set; }
+    }
+
     public class LoginController : Controller
     {
         private IAuthApp Auth = null;
@@ -25,16 +33,35 @@ namespace DLiteAuthFrame.Web.Controllers
         {
             return View();
         }
+        
+
+        [HttpPost]
+        public string Index(string name)
+        {
+            string username = Request.Form["Username"];
+            string password = Request.Form["Password"];
+            return "Welcome:" + username;
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Index2(User user)
+        {
+            return View();
+        }
 
         [HttpPost]      
-        public string CheckLogin(LoginViewModel user)
+        public ActionResult Index(LoginViewModel user)
+       // public ActionResult CheckLogin(string Name,string PassWord)
         {
             //Response.Write("<Script Language=JavaScript>alert('密码或用户名错误，请重试！');</Script>");
 
             if (Auth.Login(user.LoginCode, user.PassWord))
-                return (new AjaxResult { state = ResultType.success.ToString(), message = "登录成功。" }.ToJson());
+                return Content(new AjaxResult { state = ResultType.success.ToString(), message = "登录成功。" }.ToJson());
             else
-                return (new AjaxResult { state = ResultType.error.ToString(), message = "账号或密码错误！" }.ToJson());
+                return Content(new AjaxResult { state = ResultType.error.ToString(), message = "账号或密码错误！" }.ToJson());
+
 
         }
     }
